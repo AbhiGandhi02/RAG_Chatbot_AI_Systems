2c9f44620af7063823920f8d67023654849436eb
AbhiGandhi02 <abhigandhi0212@gmail.com>
2026-02-22 16:18:01 +0530

    Reading port properly

--- backend/main.py
+++ backend/main.py
@@ -351,4 +351,7 @@ async def health():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=PORT, reload=True)
+    import os
+
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=port)
